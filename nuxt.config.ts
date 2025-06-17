ff4c572e92c47acd36e9d1d92b3aa2436558f636
dnfwlxo11 <dnfwlxo11@gmail.com>
2025-06-17 18:27:26 +0900

    [edit]: edit html build setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,23 @@ export default defineNuxtConfig({
       ]
     }
   },
+  app: {
+    buildAssetsDir: '/nuxt',
+    head: {
+      title: '띠부로그',
+      htmlAttrs: {
+        lang: 'ko',
+        translate: 'no'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=contain' },
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'mobile-web-app-capable', content: 'yes' },
+        { name: 'google', content: 'notranslate' },
+        { name: 'theme-color', content: 'rgba(242, 241, 246, 1)', media: "(prefers-color-scheme: light)" },
+        { name: 'theme-color', content: 'rgba(0, 0, 0, 1)', media: "(prefers-color-scheme: dart)" },
+      ],
+    }
+  },
 })
